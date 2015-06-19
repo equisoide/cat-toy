4094d700b13bdf864a312e39cebe85ad97a3ecb6
Juan Cuartas <equisoide@gmail.com>
2015-06-19 01:24:06 -0500

    Added Enterprise Architect model

--- src/front-end/js/app.js
+++ src/front-end/js/app.js
@@ -1,5 +1,6 @@
 $(function () {
-	var controller = new Controller("http://192.168.137.2:3000/api/laser", $);
+	var url = "http://192.168.137.2:3000/api/laser";
+	var controller = new Controller(url, $);
 	
 	$(document).keydown(controller.onKeydown);
 	$(document).keyup(controller.onKeyup);
